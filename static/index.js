899ba2feb392958f6e5333439d8b98b2bd369000
Nishijama <nathanplociennik@gmail.com>
2022-11-17 00:08:09 +0100

    created automatically added slots placeholder photos from direct link to lorem ipsum

--- static/index.js
+++ static/index.js
@@ -16,10 +16,45 @@ themes.map(theme => {
   color.style.borderRadius = "50%";
   color.style.backgroundImage = theme;
   colorsList.appendChild(color);
-  console.log("hello");
-  return;
+  color.addEventListener("click", () => {
+    document.querySelector("body").style.backgroundImage = theme;
+    colorsList.classList.add("hidden");
+  });
 });
 
 themeBtn.addEventListener("click", () => {
   colorsList.classList.toggle("hidden");
 });
+
+// ADDING IMAGES
+const mainContainer = document.getElementById("main-container");
+let emptySlot = document.querySelector(".empty");
+emptySlot.addEventListener("click", handleAddingPhotos);
+
+function handleAddingPhotos() {
+  // when clicking on the empty slot, pull an image from lorempicsum
+  handleAddingSections();
+  emptySlot.innerHTML = `<img style="border-radius: inherit" src="https://picsum.photos/${emptySlot.clientWidth}/${emptySlot.clientHeight}/"/>`;
+  emptySlot.classList.remove("empty");
+  if (!emptySlot.nextElementSibling) handleAddingSections();
+  else emptySlot = emptySlot.nextElementSibling;
+  emptySlot.classList.add("empty");
+  emptySlot.classList.remove("hidden");
+  emptySlot.addEventListener("click", handleAddingPhotos);
+}
+
+function handleAddingSections() {
+  // if there is no 'empty' slot, create a new four-part-container and make all slots hidden except for q1
+  if (document.getElementsByClassName("empty").length === 0) {
+    const fourPartContainer = document.createElement("div");
+    fourPartContainer.classList.add("four-part-container");
+    fourPartContainer.innerHTML = `
+    <div class="add_image horizontal q1 empty">+</div>
+    <div class="add_image vertical q2 hidden">+</div>
+    <div class="add_image vertical q3 hidden">+</div>
+    <div class="add_image horizontal q4 hidden">+</div>
+  `;
+    mainContainer.appendChild(fourPartContainer);
+    emptySlot = document.querySelector(".empty");
+  }
+}
